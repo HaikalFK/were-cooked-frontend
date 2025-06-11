2191800891445ebd8d5a2c29604fbbb52c542378
HaikalFK <haikalfkarim@gmail.com>
2025-06-11 20:05:20 +0700

    Mencoba menambahkan halaman preferensi

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import DetailPage from "./pages/Detail";
 import NotFound from "./pages/NotFound";
 import AboutPage from "./pages/About";
 import Layout from "./components/Layout";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import { SearchProvider } from "./context/SearchContext";
 import "./index.css";
 import BookmarkPage from "./pages/Bookmark";
@@ -11,6 +16,7 @@ import ProfilePage from "./pages/Profile";
 import LoginPage from "./pages/Login";
 import RegisterPage from "./pages/Register";
 import EditProfilePage from "./pages/EditProfile";
+import PreferencesPage from "./pages/PreferencesPage";
 import { useEffect } from "react";
 // import UbahPassword from "./pages/UbahPassword";
 
@@ -27,21 +33,22 @@ export default function App() {
     });
   }, [location.pathname]);
   return (
-      <SearchProvider>
-        <Layout>
-          <Routes location={location} key={location.pathname}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/resep/:id" element={<DetailPage />} />
-            <Route path="/bookmark" element={<BookmarkPage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/ubah-profil" element={<EditProfilePage />} />
-            {/* <Route path="/ubah-password" element={<UbahPassword />} /> */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
-      </SearchProvider>
+    <SearchProvider>
+      <Layout>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/resep/:id" element={<DetailPage />} />
+          <Route path="/bookmark" element={<BookmarkPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/ubah-profil" element={<EditProfilePage />} />
+          <Route path="/set-preferences" element={<PreferencesPage />} />
+          {/* <Route path="/ubah-password" element={<UbahPassword />} /> */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </SearchProvider>
   );
 }
